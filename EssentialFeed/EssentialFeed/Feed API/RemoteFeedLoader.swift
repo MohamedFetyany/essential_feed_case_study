558ff5289aa3891c382cfe78c2c4140ea55e71b4
Mohamed Ibrahim <Mohamed.Ibrahim2@kfh.com>
2023-01-17 11:20:21 +0300

    Move Root and item inside FeedItemsMapper

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -55,6 +55,21 @@ public final class RemoteFeedLoader {
 
 private class FeedItemsMapper {
     
+    private struct Root: Decodable {
+        let items: [Item]
+    }
+
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+        
+        var item: FeedItem {
+            FeedItem(id: id,description: description,location: location, imageURL: image)
+        }
+    }
+    
     static func map(_ data: Data,_ response: HTTPURLResponse) throws -> [FeedItem] {
         guard response.statusCode == 200 else {
             throw RemoteFeedLoader.Error.invalidData
@@ -62,20 +77,5 @@ private class FeedItemsMapper {
         
         let root = try JSONDecoder().decode(Root.self, from: data)
         return root.items.map { $0.item }
-    }
-}
-
-private struct Root: Decodable {
-    let items: [Item]
-}
-
-private struct Item: Decodable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
-    
-    var item: FeedItem {
-        FeedItem(id: id,description: description,location: location, imageURL: image)
     }
 }
