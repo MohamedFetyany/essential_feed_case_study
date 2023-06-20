0c816d47f4ceb39d23dc396bab1df74aad22fd0d
Mohamed Ibrahim <Mohamed.Ibrahim2@kfh.com>
2023-06-20 12:46:38 +0300

    Decouple 'FeedRefreshViewController' from concrete 'FeedPresenter' dependency with a closure.

--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -14,7 +14,7 @@ public final class FeedUIComposer {
     
     public static func feedComposeWith(feedLoader: FeedLoader,imageLoader: FeedImageDataLoader) -> FeedViewController {
         let feedPresenter = FeedPresenter(feedLoader: feedLoader)
-        let refreshController = FeedRefreshController(presenter: feedPresenter)
+        let refreshController = FeedRefreshController(loadFeed: feedPresenter.loadFeed)
         let feedViewController = FeedViewController(refreshController: refreshController)
         feedPresenter.loadingView = WeakRefVirtualProxy(refreshController)
         feedPresenter.feedView = FeedViewAdapter(controller: feedViewController, imageLoader: imageLoader)
