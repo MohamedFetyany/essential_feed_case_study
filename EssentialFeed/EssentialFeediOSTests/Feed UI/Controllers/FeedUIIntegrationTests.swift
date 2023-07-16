72e71b9b8a1ec726fc3dc057d024d50055f423ed
Mohamed Ibrahim <Mohamed.Ibrahim2@kfh.com>
2023-07-16 07:37:30 +0300

    Rename 'FeedViewControllerTests' to 'FeedUIIntegrationTests since we are testing the composition of multiple UI components in integration

--- EssentialFeed/EssentialFeediOSTests/Feed UI/Controllers/FeedUIIntegrationTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/Controllers/FeedUIIntegrationTests.swift
@@ -1,5 +1,5 @@
 //
-//  FeedViewControllerTests.swift
+//  FeedUIIntegrationTests.swift
 //  EssentialFeediOSTests
 //
 //  Created by Mohamed Ibrahim on 30/03/2023.
@@ -9,7 +9,7 @@ import XCTest
 import EssentialFeediOS
 import EssentialFeed
 
-final class FeedViewControllerTests: XCTestCase {
+final class FeedUIIntegrationTests: XCTestCase {
     
     func test_feedView_hasTitle() {
         let (sut, _) = makeSUT()
--- EssentialFeed/EssentialFeediOSTests/Helpers/FeedViewControllerTests+Assertions.swift
+++ EssentialFeed/EssentialFeediOSTests/Helpers/FeedViewControllerTests+Assertions.swift
@@ -1,5 +1,5 @@
 //
-//  FeedViewControllerTests+Assertions.swift
+//  FeedUIIntegrationTests+Assertions.swift
 //  EssentialFeediOSTests
 //
 //  Created by Mohamed Ibrahim on 06/04/2023.
@@ -9,7 +9,7 @@ import XCTest
 import EssentialFeed
 import EssentialFeediOS
 
-extension FeedViewControllerTests {
+extension FeedUIIntegrationTests {
     func assertThat(
         _ sut: FeedViewController,
         isRendering feed: [FeedImage],
