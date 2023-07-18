59be062b2e31d4866fced6ad6fb71108200c5b0f
Mohamed Ibrahim <Mohamed.Ibrahim2@kfh.com>
2023-07-18 13:28:33 +0300

    Move 'FeedImageDataLoaderPresentationAdapter' to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedImageDataLoaderPresentation.swift
@@ -0,0 +1,41 @@
+//
+//  FeedImageDataLoaderPresentation.swift
+//  EssentialFeediOS
+//
+//  Created by Mohamed Ibrahim on 18/07/2023.
+//
+
+import EssentialFeed
+
+final class FeedImageDataLoaderPresentation<View: FeedImageView,Image>: FeedImageCellControllerDelegate where View.Image == Image {
+    
+    var presenter: FeedImagePresenter<View, Image>?
+    
+    private let model: FeedImage
+    private let imageLoader: FeedImageDataLoader
+    private var task: FeedImageDataLoaderTask?
+    
+    init(model: FeedImage, imageLoader: FeedImageDataLoader) {
+        self.model = model
+        self.imageLoader = imageLoader
+    }
+    
+    func didRequestImage() {
+        presenter?.didStartLoadingImageData(for: model)
+        
+        task = imageLoader.loadImageData(from: model.url) { [weak self,model] result in
+            switch result {
+            case let .success(data):
+                self?.presenter?.didStartLoadingImageData(with: data, for: model)
+                
+            case let .failure(error):
+                self?.presenter?.didStartLoadingImageData(with: error, for: model)
+            }
+        }
+    }
+    
+    func didCancelImageRequest() {
+        task?.cancel()
+    }
+}
+
