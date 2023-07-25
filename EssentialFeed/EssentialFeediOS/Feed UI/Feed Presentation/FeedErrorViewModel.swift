6680838c7c656e26e5a92e23233ad3e7321780cf
Mohamed Ibrahim <Mohamed.Ibrahim2@kfh.com>
2023-07-25 07:44:15 +0300

    Move 'FeedErrorViewModel' to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Mohamed Ibrahim on 25/07/2023.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
