ff652696ff69489d68083979ed75e5990aeb2561
Mohamed Ibrahim <Mohamed.Ibrahim2@kfh.com>
2023-07-27 07:38:19 +0300

    Move 'FeedLoadingViewModel' to separate file

---
+++ EssentialFeed/EssentialFeed/Fedd Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Mohamed Ibrahim on 27/07/2023.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
