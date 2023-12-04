fe8a248cf6934f8430aceb264d1e2603b9340431
Mohamed Ibrahim <Mohamed.Ibrahim2@kfh.com>
2023-12-04 14:28:19 +0300

    Extract system under test (SUT) creation into a factory method

--- EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -25,19 +25,24 @@ class FeedLoaderCacheDecoratorTests: XCTestCase, FeedLoaderTestCase {
     
     func test_load_deliversFeedOnLoaderSuccess() {
         let feed = uniqueFeed()
-        let loaderStub = FeedLoaderStub(result: .success(feed))
-        let sut = FeedLoaderCacheDecorator(decoratee: loaderStub)
+        let sut = makeSUT(loaderResult: .success(feed))
         
         expect(sut, toCompleteWith: .success(feed))
     }
     
     func test_load_deliversErrorOnLoaderFailure() {
-        let loaderStub = FeedLoaderStub(result: .failure(anyNSError))
-        let sut = FeedLoaderCacheDecorator(decoratee: loaderStub)
+        let sut = makeSUT(loaderResult: .failure(anyNSError))
         
         expect(sut, toCompleteWith: .failure(anyNSError))
     }
     
     // MARK:  Helpers
-
+    
+    private func makeSUT(loaderResult: FeedLoader.Result,file: StaticString = #filePath,line: UInt = #line) -> FeedLoader {
+        let loader = FeedLoaderStub(result: loaderResult)
+        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+        trackForMemoryLeaks(loader,file: file,line: line)
+        trackForMemoryLeaks(sut,file: file,line: line)
+        return sut
+    }
 }
