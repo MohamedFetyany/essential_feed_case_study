66eddf9ad43f89ffe8ef0e7a1d6a8593f277040d
Mohamed Ibrahim <Mohamed.Ibrahim2@kfh.com>
2024-01-23 07:58:21 +0300

    Add feed endpoint with "after_id param

--- EssentialFeed/EssentialFeed/Feed API/FeedEndpoint.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedEndpoint.swift
@@ -8,18 +8,19 @@
 import Foundation
 
 public enum FeedEndpoint {
-    case get
+    case get(after: FeedImage? = nil)
     
     public func url(baseURL: URL) -> URL {
         switch self {
-        case .get:
+        case let .get(image):
             var component = URLComponents()
             component.scheme = baseURL.scheme
             component.host = baseURL.host()
             component.path = baseURL.path() + "/v1/feed"
             component.queryItems = [
-                URLQueryItem(name: "limit", value: "10")
-            ]
+                URLQueryItem(name: "limit", value: "10"),
+                image.map { URLQueryItem(name: "after_id", value: $0.id.uuidString) }
+            ].compactMap { $0 }
             return component.url!
         }
     }
