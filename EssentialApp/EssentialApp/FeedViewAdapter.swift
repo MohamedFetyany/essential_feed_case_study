6efde96cdb39218a3171e014f6f87744d0a17dee
Mohamed Ibrahim <Mohamed.Ibrahim2@kfh.com>
2024-01-30 08:16:20 +0300

    Reuse existing cell controllers to avoid unnecessary reallocation of resources

--- EssentialApp/EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/EssentialApp/FeedViewAdapter.swift
@@ -16,22 +16,32 @@ final class FeedViewAdapter: ResourceView {
     private typealias ImageDataPresentationAdapter = LoadResourcePresentationAdapter<Data,WeakRefVirtualProxy<FeedImageCellController>>
     private typealias LoadMorePresentationAdapter = LoadResourcePresentationAdapter<Paginated<FeedImage>,FeedViewAdapter>
     
+    private var currentFeed: [FeedImage: CellController]
     private weak var controller: ListViewController?
     private let imageLoader: ((URL) -> FeedImageDataLoader.Publisher)
     private let selection: ((FeedImage) -> Void)
     
     init(
+        currentFeed: [FeedImage: CellController] = [:],
         controller: ListViewController,
         imageLoader: @escaping ((URL) -> FeedImageDataLoader.Publisher),
         selection: @escaping ((FeedImage) -> Void)
     ) {
+        self.currentFeed = currentFeed
         self.controller = controller
         self.imageLoader = imageLoader
         self.selection = selection
     }
     
     func display(_ viewModel: Paginated<FeedImage>) {
+        guard let controller else { return }
+        
+        var currentFeed = self.currentFeed
         let feed: [CellController] = viewModel.items.map { model in
+            if let controller = currentFeed[model] {
+                return controller
+            }
+            
             let adapter = ImageDataPresentationAdapter(loader: { [imageLoader]  in imageLoader(model.url) })
             let view = FeedImageCellController(viewModel: FeedImagePresenter.map(model),delegate: adapter,selection: { [selection] in
                 selection(model)
@@ -42,20 +52,26 @@ final class FeedViewAdapter: ResourceView {
                 errorView: WeakRefVirtualProxy(view),
                 mapper: UIImage.tryMake
             )
-            return CellController(id: model,view)
+            let controller = CellController(id: model,view)
+            currentFeed[model] = controller
+            return controller
         }
         
         guard let loadMorePublisher = viewModel.loadMorePublisher else {
-            controller?.display(feed)
+            controller.display(feed)
             return
         }
         
         let loadMoreAdapter = LoadMorePresentationAdapter(loader: loadMorePublisher)
         let loadMore = LoadMoreCellController(callback: loadMoreAdapter.loadResource)
-        loadMoreAdapter.presenter = LoadResourcePresenter(resourceView: self,loadingView: WeakRefVirtualProxy(loadMore),errorView: WeakRefVirtualProxy(loadMore))
+        loadMoreAdapter.presenter = LoadResourcePresenter(
+            resourceView: FeedViewAdapter(currentFeed: currentFeed, controller: controller, imageLoader: imageLoader, selection: selection),
+            loadingView: WeakRefVirtualProxy(loadMore),
+            errorView: WeakRefVirtualProxy(loadMore)
+        )
         
         let loadMoreSection = [CellController(id: UUID(), loadMore)]
-        controller?.display(feed,loadMoreSection)
+        controller.display(feed,loadMoreSection)
     }
 }
 
