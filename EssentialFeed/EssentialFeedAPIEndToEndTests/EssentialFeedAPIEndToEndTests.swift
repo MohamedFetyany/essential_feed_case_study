2136955e777f7fe171a5c90405889899d3680990
Mohamed Ibrahim <Mohamed.Ibrahim2@kfh.com>
2024-03-10 10:30:58 +0300

    Remove 'FeedImageDataLoader.Result'

--- EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -74,12 +74,12 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
         return receivedResult
     }
     
-    private func getFeedImageDataResult(file: StaticString = #filePath,line: UInt = #line) -> FeedImageDataLoader.Result? {
+    private func getFeedImageDataResult(file: StaticString = #filePath,line: UInt = #line) -> Result<Data,Error>? {
         let url = feedTestServerURL.appending(path: "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")
         let client = ephemeralClient()
         
         let exp = expectation(description: "Wait for load completion")
-        var receviedResult: FeedImageDataLoader.Result?
+        var receviedResult: Result<Data,Error>?
         
         _ = client.get(from: url, completion: { result in
             receviedResult = result.flatMap({ data,response in
